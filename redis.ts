ec948b8d742655dd130e41bff7d921f7084d3660
Reiss Melissa <glitzereinhorn270487/gmail.com>
2025-06-29 15:36:05 +0200

    Fix:Korrigiere TLS-Fehler in api/add-position/route.js

--- redis.ts
+++ redis.ts
@@ -2,11 +2,15 @@
 import Redis from "ioredis";
 
 const redis = new Redis("redis://default:qtp3dkzcGFSMr9cMOLSSKKzrGviwq2XF@redis-15333.c55.eu-central-1-1.ec2.redns.redis-cloud.com:15333");
-
+host: "redis-15333.c55.eu-central-1-1.ec2.redns.redis-cloud.com",
+  port: 15333,
+  username: "default", // falls nötig
+  password: "qtp3dkzcGFSMr9cMOLSSKKzrGviwq2XF",
+  tls: {}
 redis.set("ping", "pong").then(() => {
   return redis.get("ping");
 }).then(result => {
-  console.log("✅ Redis antwortet:", result);
+  console.log("redis://default:qtprdkzcGFSMr9cMOLSSKKzrGviwq2XF@redis-15333.c55.eu-central-1-1.ec2.redns.redis-cloud.com:15333", process.env.REDIS_URL);
   redis.disconnect();
 }).catch(err => {
   console.error("❌ Fehler bei Redis-Verbindung:", err);
