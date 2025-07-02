7a11f5016e4f2e234f3dea13b0a9f3283180e41f
Reiss Melissa <glitzereinhorn270487/gmail.com>
2025-07-02 12:13:12 +0200

    fix: type/token.ts

--- app/types/token.ts
+++ app/types/token.ts
@@ -2,20 +2,20 @@
 
 export interface TradeCandidate {
   address: string;
-  category?: string;
-  score?: number;
+  category: string;
+  score: number;
   risk?: number;
-  baseScore?: number;
-  totalScore?: number;
+  baseScore: number;
+  totalScore: number;
   token?: string;
   notice?: string;
   narrative?: string;
   type?: string;
-  follower?: number;
-  winRate?: number;
-  rugRate?: number;
-  symbol?: string;
-  name?: string;
+  follower: number;
+  winRate: number;
+  rugRate: number;
+  symbol: string;
+  name: string;
   candidate?: string;
   
 
