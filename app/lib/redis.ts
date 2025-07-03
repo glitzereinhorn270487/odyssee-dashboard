bd4006e3f0b8662fd54d5708e9448bdc12f07693
Reiss Melissa <glitzereinhorn270487/gmail.com>
2025-07-03 18:16:15 +0200

    Fix REDIS_URL and delete CUSTOM_REDIS_URL

--- app/lib/redis.ts
+++ app/lib/redis.ts
@@ -1,8 +1,8 @@
 // lib/redis.ts
 import Redis from "ioredis";
 
-const redis = new Redis(process.env.CUSTOM_REDIS_URL!, {
-  tls: {},
+const redis = new Redis(process.env.REDIS_URL!, {
+
   maxRetriesPerRequest: 5,
   enableOfflineQueue: false,
 });
