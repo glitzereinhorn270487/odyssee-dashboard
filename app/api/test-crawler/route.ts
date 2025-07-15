ad6f5addecc4b24c2c0c22759905172fb746c3bf
Reiss Melissa <glitzereinhorn270487/gmail.com>
2025-07-15 19:31:33 +0200

    Fix error

--- app/api/test-crawler/route.ts
+++ app/api/test-crawler/route.ts
@@ -1,6 +1,12 @@
 // app/api/test-crawler/route.ts
 import { runCrawler } from "@/lib/crawler/crawler";
+
 export async function GET() {
+  console.log("[TEST-CRAWLER] Endpoint wurde aufgerufen");
+
   await runCrawler();
+
+  console.log("[TEST-CRAWLER] runCrawler wurde beendet");
+
   return Response.json({ success: true });
 }
