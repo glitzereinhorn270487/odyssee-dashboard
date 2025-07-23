e53a814120ccb9f43865d131f56ab35182b59211
Reiss Melissa <glitzereinhorn270487/gmail.com>
2025-07-23 17:35:36 +0200

    Fix error

--- app/lib/redisClient.ts
+++ app/lib/redisClient.ts
@@ -1,7 +1,7 @@
 // lib/redisClient.ts
 import Redis from "ioredis";
 
-const redisUrl = process.env.UPSTASH_REDIS_REST_URL || 'redis://localhost:6379';
+const redisUrl = process.env.UPSTASH_REDIS_REST_URL || 'https://cheerful-whale-55517.upstash.io' || 'redis://localhost:6379';
 
 const redis = new Redis(redisUrl || "redis://127.0.0.1:6379");
 
